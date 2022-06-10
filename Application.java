8974eef212eee7c89f79b4f36b9cd3177181b24d
Noli <noliliao13@gmail.com>
2022-06-10 21:01:47 +0800

    update k

--- Application.java
+++ Application.java
@@ -5,7 +5,7 @@ import org.tron.trident.proto.Response;
 
 public class Application {
     public static void main(String[] args) {
-        ApiWrapper wrapper = ApiWrapper.ofMainnet("1111111111222222222233333333334444444444555555555566666666667777", "f215a852-a3e1-d698c882d891");
+        ApiWrapper wrapper = ApiWrapper.ofMainnet("1111111111222222222233333333334444444444555555555566666666667777", "fb15a852-a3e1-4310-9b7d-d698c882d89a");
         Response.Account account = wrapper.getAccount("TGLp89yzK2MQha1CRaiaJufPyZRFFnvonW");
         System.out.println(account.getBalance());
         Response.AssetIssueList list = wrapper.getAssetIssueByAccount("TSAhByujvgHgFgL9U48NiUBjkLasP2zCGb");
